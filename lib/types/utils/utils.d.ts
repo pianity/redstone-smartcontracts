754db50b5ab154e0585fcf2465585cf564d1e1ac
Eyal Chojnowski <eyal.choj@gmail.com>
2022-05-06 17:34:13 +0200

    chore: add lib/ for distribution

---
+++ lib/types/utils/utils.d.ts
@@ -0,0 +1,19 @@
+export declare const sleep: (ms: number) => Promise<void>;
+export declare const deepCopy: (input: unknown, useFastCopy?: boolean) => any;
+export declare const mapReplacer: (key: unknown, value: unknown) => unknown;
+export declare const mapReviver: (key: unknown, value: any) => any;
+export declare const asc: (a: number, b: number) => number;
+export declare const ascS: (a: string, b: string) => number;
+export declare const desc: (a: number, b: number) => number;
+export declare const descS: (a: string, b: string) => number;
+export declare function timeout(s: number): {
+    timeoutId: number;
+    timeoutPromise: Promise<any>;
+};
+export declare function stripTrailingSlash(str: string): string;
+export declare class CustomError<T> extends Error {
+    kind: T;
+    originalError?: unknown;
+    constructor(kind: T, message?: string, originalError?: unknown);
+}
+//# sourceMappingURL=utils.d.ts.map
